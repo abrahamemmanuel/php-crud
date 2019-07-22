c32b1fab8533c4b2d6fb8fc2e125c45fd1f6a5d0
Abraham Emmnauel <sundayemmanuelabraham@gmail.com>
2019-07-22 16:16:00 +0100

    SESSION MESSAGE: added session message to display action performed by the user on database records

--- index.php
+++ index.php
@@ -14,6 +14,16 @@
 
 <body>
 	<?php require_once 'process.php';?>
+
+	<?php if(isset($_SESSION['message'])):?>
+
+	<div class="alert alert-<?=$_SESSION['msg_type']?>">
+		<?php
+    echo $_SESSION['message'];
+    unset($_SESSION['message']);
+  ?>
+	</div>
+	<?php endif ?>
 	<div class="container">
 		<?php
     $mysqli = new mysqli('localhost', 'emmy', '@Emmanuel2295', 'crud') or die(mysqli_error($mysqli));
--- process.php
+++ process.php
@@ -1,5 +1,8 @@
 <?php
 
+// start session
+session_start();
+
 // Connect to database
 $mysqli = new mysqli('localhost', 'emmy', '@Emmanuel2295', 'crud') or die(mysqli_error($mysqli));
 
@@ -13,7 +16,13 @@ if(isset($_POST['save'])){
   // Insert record into databse
   $mysqli->query("INSERT INTO data (name, location) VALUES('$name', '$location')") or die($mysqli->error);
 
+  // Set session message and type for saving records
+  // Set session message and type for deleting records
+  $_SESSION['message'] = "Record has been saved!";
+  $_SESSION['msg_type'] = "success";
 
+  // Redirect users back to index.php
+  header("location: index.php");
 }
 
 // Check if the delete button has been clicked
@@ -22,6 +31,13 @@ if(isset($_GET['delete'])){
 
   // Delete record from database
   $mysqli->query("DELETE From data WHERE id=$id") or die($mysqli->error);
+
+  // Set session message and type for deleting records
+  $_SESSION['message'] = "Record has been deleted!";
+  $_SESSION['msg_type'] = "danger";
+
+  // Redirect users back to index.php
+  header("location: index.php");
 }
 
 
